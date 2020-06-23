90d7c85349629ba45604fee7a0b47997f2857832
satyam sheel <sheelsatyam@ymail.com>
2020-06-23 11:30:52 +0530

    Update bitwise.java

--- DS/src/com/satyam/practise/bitwise.java
+++ DS/src/com/satyam/practise/bitwise.java
@@ -1,7 +1,7 @@
 package com.satyam.practise;
 import java.util.*;
 
-public class bitwise {
+public class bitwise_282A {
 	
 	public static void main(String args[]) {
 		Scanner sc =new Scanner(System.in);
