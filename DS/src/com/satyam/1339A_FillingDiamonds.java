a3ea71eed1a470b683a6de1bf3b843c37e6dc9ac
satyam sheel <sheelsatyam@ymail.com>
2020-07-30 13:52:16 +0530

    Add files via upload

---
+++ DS/src/com/satyam/1339A_FillingDiamonds.java
@@ -0,0 +1,16 @@
+//package com.satyam.codeforces.distributer;
+
+import java.util.*;
+public class FillingDiamonds {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int t=sc.nextInt();
+		while(t-->0) {
+			int p=sc.nextInt();
+			System.out.println(p);
+		}
+		sc.close();
+	}
+
+}
